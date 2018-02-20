72ca22a4eefe0d913c50fafeba63d6841ebfa7c9
reallivedinosaur <joe.hartrick@gmail.com>
2018-02-20 16:20:57 -0500

    updated controlpad code

--- src/chichi/wishbone/controlpads.ts
+++ src/chichi/wishbone/controlpads.ts
@@ -5,27 +5,28 @@ import { WishboneIO, Wishbone } from './wishbone';
 export interface KeyboardHandlingComponent {
     onkeydown(event: any): void;
     onkeyup(event: any): void;
-  }
-  
-  
+}
+
 const setupKeyboards = (comp: KeyboardHandlingComponent) => (wishbone?: Wishbone) => {
     // higher order function creates keyboard event handlers and binds them to angular component
     const padOne: Pads.WishBoneControlPad = Pads.createControlPad(defaultBindings)('one');
     // TODO: setup some default player two bindings
     // const padTwo: Pads.WishBoneControlPad = Pads.createControlPad(defaultBindings)('two');
-    wishbone.padOne.ControlPad
+
     comp.onkeydown = (event) => Pads.handleKeyDownEvent(padOne, event);
     comp.onkeyup = (event) => Pads.handleKeyUpEvent(padOne, event);
-    wishbone.padOne.ControlPad.getPadState = () => padOne.padOneState;
 
+    wishbone.padOne.getPadState = () => {
+        return padOne.padOneState;
+    };
     // Then returns a function which can attach polling functions to a ChiChiIO object
     return (io: WishboneIO) => {
         const newIo = Object.assign({}, io);
 
         return Object.freeze(newIo);
-    }
+    };
 };
 
 export const WishboneControlPads = {
     setupKeyboards
-}
+};
